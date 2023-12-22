d7fa4517b7502914b6386ff6282576f866c00840
Mengyu Bai <mengyu.bai@gmail.com>
2023-12-22 12:08:35 -0500

    finish d7 p2

--- 2024/day_07_camel_cards.py
+++ 2024/day_07_camel_cards.py
@@ -43,6 +43,38 @@ class Hand:
     def hand_type(self) -> str:
         return self._evaluate_hand_type()
 
+    def _evaluate_hand_type_joker(self) -> str:
+        card_count = {}
+        joker_count = 0
+        print(f"{self._cards}")
+        if self._cards == "JJJJJ":
+            return "five_of_a_kind"
+        for card in self._cards:
+            if card == "J":
+                joker_count += 1
+            elif card in card_count:
+                card_count[card] += 1
+            else:
+                card_count[card] = 1
+        combos = sorted(list(card_count.values()), reverse=True)
+        combos[0] = combos[0] + joker_count
+        if 5 in combos:
+            return "five_of_a_kind"
+        elif 4 in combos:
+            return "four_of_a_kind"
+        elif 3 in combos and 2 in combos:
+            return "full_house"
+        elif 3 in combos:
+            return "three_of_a_kind"
+        elif combos.count(2) == 2:
+            return "two_pair"
+        elif 2 in combos:
+            return "one_pair"
+        else:
+            return "high_card"
+
+    def hand_type_joker(self) -> str:
+        return self._evaluate_hand_type_joker()
 
 class CamelDeck:
     def __init__(self, hands_strings: list[str]) -> None:
@@ -98,6 +130,48 @@ class CamelDeck:
         return self._deck
 
 
+    def _sort_hands_by_rankings_joker(self) -> list[Hand]:
+        type_ranking = {
+            "high_card": 1,
+            "one_pair": 2,
+            "two_pair": 3,
+            "three_of_a_kind": 4,
+            "full_house": 5,
+            "four_of_a_kind": 6,
+            "five_of_a_kind": 7,
+        }
+        self.all_hands_with_type_rank = {}
+        for hand in self._deck:
+            hand_type = hand.hand_type_joker()
+            type_rank = type_ranking[hand_type]
+            if type_rank in self.all_hands_with_type_rank.keys():
+                self.all_hands_with_type_rank[type_rank].append(hand)
+            else:
+                self.all_hands_with_type_rank[type_rank] = [hand]
+        self.all_hands_with_type_rank = dict(
+            sorted(self.all_hands_with_type_rank.items())
+        )
+
+        ranks_ordered = ["J"] + [str(n) for n in range(2, 10)] + list("TQKA")
+        rank_ranking = {key: value for key, value in zip(ranks_ordered, range(1, 14))}
+
+        def get_rank(hand: Hand) -> list[int]:
+            chars = hand._cards
+            return [rank_ranking.get(c, 0) for c in chars]
+
+        for type_ranking, hands in self.all_hands_with_type_rank.items():
+            ranked_hands = sorted(hands, key=get_rank)
+            self.all_hands_with_type_rank[type_ranking] = ranked_hands
+
+        new_deck_order = []
+        for key in self.all_hands_with_type_rank:
+            new_deck_order.extend(self.all_hands_with_type_rank[key])
+
+        self._deck = new_deck_order
+
+        return self._deck
+
+
 def solve_part_1(lines: list[str]) -> int:
     deck = CamelDeck(lines)
     deck._sort_hands_by_rankings()
@@ -109,6 +183,18 @@ def solve_part_1(lines: list[str]) -> int:
     print(f"Part1: {winnings}")
     return winnings
 
+def solve_part_2(lines: list[str]) -> int:
+    deck = CamelDeck(lines)
+    deck._sort_hands_by_rankings_joker()
+
+    bid_by_rank = zip([hand._bid for hand in deck._deck], range(1, len(deck._deck) + 1))
+
+    winnings = sum(bid * rank for bid, rank in bid_by_rank)
+
+    print(f"Part2: {winnings}")
+    return winnings
+
 
 if __name__ == "__main__":
     solve_part_1(data)
+    solve_part_2(data)
--- 2024/test_camel_cards.py
+++ 2024/test_camel_cards.py
@@ -1,6 +1,6 @@
 import unittest
 
-from day_07_camel_cards import CamelDeck, Hand, solve_part_1
+from day_07_camel_cards import CamelDeck, Hand, solve_part_1, solve_part_2
 
 with open("data/07_sample.txt") as f:
     sample_data = f.readlines()
@@ -48,6 +48,27 @@ class TestCamelCards(unittest.TestCase):
     def test_part_1_answer(self):
         actual = solve_part_1(sample_data)
         self.assertEqual(actual, 6440)
+
+    def test_hand_type_is_assumed_correctly_with_joker(self):
+        self.assertEqual(Hand("32T3K", 1).hand_type_joker(), "one_pair")
+        self.assertEqual(Hand("TJ5T5", 1).hand_type_joker(), "full_house")
+        self.assertEqual(Hand("KK677", 1).hand_type_joker(), "two_pair")
+        self.assertEqual(Hand("QQQJA", 1).hand_type_joker(), "four_of_a_kind")
+        self.assertEqual(Hand("AAAAA", 1).hand_type_joker(), "five_of_a_kind")
+        self.assertEqual(Hand("444JJ", 1).hand_type_joker(), "five_of_a_kind")
+        self.assertEqual(Hand("3A7KT", 1).hand_type_joker(), "high_card")
+
+    def test_hands_are_sorted_by_type_correctly_joker(self):
+        actual = CamelDeck(sample_data)._sort_hands_by_rankings_joker()
+        self.assertEqual(actual[0]._cards, "32T3K")
+        self.assertEqual(actual[1]._cards, "KK677")
+        self.assertEqual(actual[2]._cards, "T55J5")
+        self.assertEqual(actual[3]._cards, "QQQJA")
+        self.assertEqual(actual[4]._cards, "KTJJT")
+
+    def test_part_2_answer(self):
+        actual = solve_part_2(sample_data)
+        self.assertEqual(actual, 5905)
 
 
 if __name__ == "__main__":
